84e8b417142421b3c760cfe96933080dab5ab7ff
Arpan <Arpan@gmail.com>
2021-08-16 13:53:50 +0530

    second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { AppetizerComponent } from './menu/appetizer/appetizer.component';
+import { BeveragesComponent } from './menu/beverages/beverages.component';
+import { DessertComponent } from './menu/dessert/dessert.component';
+import { MainCourseComponent } from './menu/main-course/main-course.component';
+import { MenuComponent } from './menu/menu.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'menu' , pathMatch: 'full'},
+  {path: 'menu', component: MenuComponent,  children:
+  [
+    { path : '' , redirectTo:  'appetizers' , pathMatch:  'full'},
+  { path:'main-course' ,component: MainCourseComponent },
+  { path:'appetizers' ,component: AppetizerComponent },
+  { path:'beverages' ,component: BeveragesComponent },
+  { path:'desserts' ,component: DessertComponent }]},
+  {path: 'about' , component: AboutComponent},
+  {path: 'reserve', component: RegisterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { MenuComponent } from './menu/menu.component';
+import { AboutComponent } from './about/about.component';
+import { RegisterComponent } from './register/register.component';
+import { MainCourseComponent } from './menu/main-course/main-course.component';
+import { AppetizerComponent } from './menu/appetizer/appetizer.component';
+import { BeveragesComponent } from './menu/beverages/beverages.component';
+import { DessertComponent } from './menu/dessert/dessert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CarouselComponent,
+    MenuComponent,
+    AboutComponent,
+    RegisterComponent,
+    MainCourseComponent,
+    AppetizerComponent,
+    BeveragesComponent,
+    DessertComponent
   ],
   imports: [
     BrowserModule,
